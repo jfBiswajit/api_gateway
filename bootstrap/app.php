51d9f324ab304e9448d89a125b47c790902efbe4
Biswajit Biswas <jfbiswajit@gmail.com>
2020-10-03 09:25:24 +0600

    Add: CustomExternalService

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,12 @@ $app->withFacades();
 
 $app->withEloquent();
 
+/**
+ * register config files
+ */
+
+$app->configure('services');
+
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
