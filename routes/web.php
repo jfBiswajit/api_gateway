29386868e86187408599ea22f6579043df9b3ade
Biswajit Biswas <jfbiswajit@gmail.com>
2020-10-03 08:56:42 +0600

    Add: route for authors

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+$router->get('/authors', 'BookController@index');
+$router->post('/authors', 'BookController@store');
+$router->get('/authors/{id}', 'BookController@show');
+$router->put('/authors/{id}', 'BookController@update');
+$router->patch('/authors/{id}', 'BookController@update');
+$router->delete('/authors/{id}', 'BookController@destroy');
+
 $router->get('/books', 'BookController@index');
 $router->post('/books', 'BookController@store');
 $router->get('/books/{id}', 'BookController@show');
