3d42845193c09516dc1194432bf088eb2d5d5da7
Biswajit Biswas <jfbiswajit@gmail.com>
2020-10-09 20:48:47 +0600

    Add: install lumen passport

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -98,8 +98,9 @@ $app->configure('app');
 */
 
 // $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Laravel\Passport\PassportServiceProvider::class);
+$app->register(Dusterio\lumenPassport\PassportServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
