280c1d6d73ce18e9a02ead5b00bea59331e45af6
Biswajit Biswas <jfbiswajit@gmail.com>
2020-10-10 09:22:24 +0600

    Add: secrect for microservice

--- app/services/BookService.php
+++ app/services/BookService.php
@@ -9,10 +9,12 @@ class BookService
   use ConsumeExternalService;
 
   public $baseUri;
+  public $secret;
 
   public function  __construct()
   {
     $this->baseUri = config('services.books.base_uri');
+    $this->secret = config('services.books.secret');
   }
 
   public function obtainBooks()
--- config/services.php
+++ config/services.php
@@ -1,6 +1,12 @@
 <?php
 
 return [
-  'authors' => ['base_uri' => env('AUTHORS_SERVICE_BASE_URI')],
-  'books' => ['base_uri' => env('BOOKS_SERVICE_BASE_URI')],
+  'authors' => [
+    'base_uri' => env('AUTHORS_SERVICE_BASE_URI'),
+    'secret' => env('AUTHOR_SERVICE_SECRET')
+  ],
+  'books' => [
+    'base_uri' => env('BOOKS_SERVICE_BASE_URI'),
+    'secret' => env('BOOKS_SERVICE_SECRET')
+    ],
 ];
